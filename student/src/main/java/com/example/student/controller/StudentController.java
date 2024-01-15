f2533c46cc85f25f3be4f810dab3e8953170f71c
beckgo34 <beckgo34@naver.com>
2024-01-15 11:29:12 +0900

    학생등록 서비스까지

--- student/src/main/java/com/example/student/controller/StudentController.java
+++ student/src/main/java/com/example/student/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.student.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.student.dto.StudentDTO;
+import com.example.student.service.StudentService;
 
 @Controller
 public class StudentController {
+	
+	@Autowired
+	private StudentService studentService;
+	
 	// 학생등록 화면 출력
 	@GetMapping("/sava")
 	public String sava() {
@@ -21,6 +27,8 @@ public class StudentController {
 	@PostMapping("/save")
 	public String save(@ModelAttribute StudentDTO studentDTO) {
 		System.out.println(studentDTO);
+		// StudentService 클래스의 save 메서드로 StudentDTO 객체 전달
+		studentService.save(studentDTO);
 		return "index";
 	}
 }
